7e64acc01e13dda696032fcf70f5cb7650cfd900
eid abdirahman <shucaybcabdi2017@gmail.com>
2025-06-22 16:55:31 +0300

    ..

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -17,7 +17,7 @@ export const authUser = async (req: Request, res: Response): Promise<void> => {
       res.cookie("token", token, {
         httpOnly: true,           // Can’t be accessed via JS on client
         secure: process.env.NODE_ENV === "production", // Only over HTTPS
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
