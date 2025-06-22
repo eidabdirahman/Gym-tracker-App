7087d1c8c022f183179911246cb1a08ff4189a9d
eid abdirahman <shucaybcabdi2017@gmail.com>
2025-06-22 13:32:57 +0300

    ..

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,6 @@ app.use(cors({
   origin: ["http://localhost:5173", "https://gym-tracker-app-frontend.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 
