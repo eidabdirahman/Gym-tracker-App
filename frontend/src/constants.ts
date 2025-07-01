66a2b95e98c477178e11731c9c8530b600f6b0b2
eid abdirahman <shucaybcabdi2017@gmail.com>
2025-07-01 17:37:02 +0300

    .

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = '';
+export const BASE_URL = 'https://gym-tracker-app-serverside.vercel.app';
 
 export const REPORTS_URL = `${BASE_URL}/api/reports`;
 export const USERS_URL = `${BASE_URL}/api/users`;
